5ddce4fd33e1d9047204dd7411a362033188943b
Side-Stick <66842022+Side-Stick@users.noreply.github.com>
2021-08-31 22:05:42 +0800

    Create top_index.h

---
+++ top_index.h
@@ -0,0 +1,29 @@
+#ifndef top_index_h
+#define top_index_h
+
+//******************************************************************
+//************* REMEMBER to adjust these parameters!!!**************
+//******************************************************************
+#define __WORDSIZE  64  //current platform is 64 bit, Intel x86-64.
+//*********************** END of parameters ************************
+
+//const(s) define, such as Error_Codes:
+#include "const_bmpSystem.h"
+#include "const_ErrorCodes.h"
+
+//struct(s) or data type(s):
+#include "struct_bmpFileStructure.h"
+
+//function(s):
+bool get_system_endian (void);  //basic_SetUp.cpp
+void initial (void);    //basic_SetUp.cpp
+bmpData ReadBmp (char* bmp_file_path);  //basic_bmp_io.cpp
+int SaveBmp (char* save_file_path, bmpData bmp_image); //basic_bmp_io.cpp
+void DeleteBmpData (bmpData bmp_image); //basic_bmp_io.cpp
+
+//class(es):
+#include "BitMapImg_BaseClass.hpp"
+#include "ColorTrans_Class.hpp"
+#include "GeometryTrans_Class.hpp"
+
+#endif /* top_index_h */
