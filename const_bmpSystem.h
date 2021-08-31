1c1beaa2f5730435fa390bd74da691a9f71ac720
Side-Stick <66842022+Side-Stick@users.noreply.github.com>
2021-08-31 21:53:39 +0800

    Create const_bmpSystem.h

---
+++ const_bmpSystem.h
@@ -0,0 +1,11 @@
+#ifndef const_bmpSystem_h
+#define const_bmpSystem_h
+//some const defined by (windows) system about BMP files.
+
+// bmp BitMapInfoHeader, biCompression
+#define BI_RGB  0   //uncompressed
+#define BI_RLE8 1
+#define BI_RLE4 2
+#define BI_BITFIELDS    3
+
+#endif /* const_bmpSystem_h */
